325773692531a14e90126e41b74c5639eb95c43b
BruCamps <brunaevelyncampos095@gmail.com>
2025-05-28 11:51:24 +0000

    fix(defs-verificar): Fix conditions and add library re

--- verificar.py
+++ verificar.py
@@ -2,7 +2,7 @@ import re
 import defs
 
 # Regular Expression para o padrão de email
-padrao_email = r'[a-zA-Z0-9_.+-]+@ufrpe\.br'
+padrao_email = r'[a-zA-Z0-9.]+@ufrpe\.br'
 
 # Verificação do campo Nome
 def Nome():
@@ -15,37 +15,49 @@ def Nome():
         # Verifica se o campo foi preenchido
         if not nome:
             print("\033[31mCampo obrigatório!\033[0;0m")
+            continue
+        elif any(i.isdigit() for i in nome):
+            print("\033[31mNão é válido utilização de números.\033[0;0m")
+            continue
+        elif(re.search(r'[^a-zA-Z0-9]', nome)):
+            print("Possui caracteres. Não é aceito.")    
+            continue
         # Verifica se o nome contém algo que não seja letra
-        if (re.match(r'[^a-zA-Z]', nome)):
-            print("\033[31mDigite um nome válido.\033[0;0m")
+        elif (re.search(r'[a-zA-Z]', nome)):
+            # Retorna o nome caso ele seja válido
+            return nome
 
-        # Retorna o nome caso ele seja válido
-        return nome
+        print("\033[31mInsira um nome válido.\033[0;0m")
 
 # Verificação do campo Senha
 def Senha():
     # Repetição para verificar a senha até que atenda ao padrão
     while True:
-        # Variável que recebe a senha
-        senha = input("Informe sua senha: ").strip()
         # Instruções de senha
         print("\033[32mSua senha deve conter pelo menos 6 caracteres, uma letra maiúscula e um caractere especial.\033[0;0m")
-
+        # Variável que recebe a senha
+        senha = input("Informe sua senha: ").strip()
+        
         # Verifica se o campo foi preenchido
         if not senha:
             print("\033[31mCampo obrigatório!\033[0;0m")
+            continue
         # Verifica se a senha contém espaços
         if " " in senha:
-            print("\033[31mA senha não pode conter espaços!\033[0;0m")
+            print("\033[31mA senha não pode conter espaços!\033[0;00m]")
+            continue
         # Verifica se a senha tem menos de 6 caracteres
         if len(senha) < 6:
             print("\033[31mA senha deve ter no mínimo 6 caracteres!\033[0;0m")
-        # Verifica se a senha contém uma letra maiúscula
+            continue
+        # Verifica se a senha contém uma letra continuea
         if not re.search(r'[A-Z]', senha):
             print("\033[31mA senha deve conter pelo menos uma letra maiúscula!\033[0;0m")
+            continue
         # Verifica se a senha contém um caractere especial
         if not re.search(r'[^a-zA-Z0-9]', senha):
-            print("\033[31mA senha deve conter pelo menos um caractere especial!\033[0;0m")
+            print("\033[31mA senha deve conter pelo menos um caractere especial!\033[0;00m")
+            continue
 
         # Retorna a senha caso ela seja válida 
         return senha
@@ -63,17 +75,21 @@ def NovaSenha(senha_atual):
         if not senha_input:
             return senha_atual
         # Verifica se a senha contém espaços
-        if " " in senha_input:
+        elif " " in senha_input:
             print("\033[31mA senha não pode conter espaços!\033[0;0m")
+            continue
         # Verifica se a senha tem menos de 6 caracteres
         if len(senha_input) < 6:
             print("\033[31mA senha deve ter no mínimo 6 caracteres!\033[0;0m")
+            continue
         # Verifica se a senha contém uma letra maiúscula
-        if not re.search(r'[A-Z]', senha_input):
+        if re.search(r'[^A-Z]', senha_input):
             print("\033[31mA senha deve conter pelo menos uma letra maiúscula!\033[0;0m")
-        # Verifica se a senha contém um caractere especial
+            continue
+        # Verifica se a senha não contém um caractere especial
         if not re.search(r'[^a-zA-Z0-9]', senha_input):
             print("\033[31mA senha deve conter pelo menos um caractere especial!\033[0;0m")
+            continue
 
         # Retorna a nova senha caso ela seja válida
         return senha_input
@@ -88,9 +104,11 @@ def ConfirmaSenha(senha):
         # Verifica se o campo foi preenchido
         if not confirmaSenha:
             print("\033[31mCampo obrigatório!\033[0;0m")
+            continue
         # Verifica se as senhas são iguais
         elif confirmaSenha != senha:
-            print("\033[31mAs senhas são diferentes!\033[0;0m")
+            print("\033[31mAs senhas são diferentes!\033[0;00m")
+            continue
 
         # Retorna a senha caso ela seja válida
         return confirmaSenha
@@ -105,12 +123,21 @@ def Email():
         # Verifica se o campo foi preenchido
         if not email:
             print("\033[31mCampo obrigatório!\033[0;0m")
+            continue
+        # Verifica se o email possui espaços
+        if " " in email:
+            print("\033[31mO email não pode conter espaços!\033[0;0m")
+            continue
+        elif not (re.search(r'[^.]', email)):
+            print("\033[31mO email não pode conter outros caracteres especiais exceto .\033[0;0m")
+            continue
         # Verifica se o email está em um formato válido e retorna seu valor
         elif (re.match(padrao_email, email)):
             return email
 
         # Mensagem para informar que o email é inválido
-        print("\033[31mEmail inválido. O domínio deve ser '@ufrpe.br'.\033[0;0m")
+        print("\033[31mEmail inválido. O email deve ser válido e do domínio '@ufrpe.br'.\033[0;0m")
+        continue
 
 # Verificação do campo Novo Email
 def NovoEmail(email_atual):
@@ -127,7 +154,8 @@ def NovoEmail(email_atual):
             return email
 
         # Mensagem para informar que o email é inválido
-        print("\033[31mEmail inválido. O domínio deve ser '@ufrpe.br'.\033[0;0m")
+        print("\033[31mEmail inválido. O email deve ser válido e do domínio '@ufrpe.br'.\033[0;0m")
+        continue
 
 # Verificação do campo Usuário
 def Usuario():
@@ -139,12 +167,15 @@ def Usuario():
         # Verifica se o campo foi preenchido
         if not usuario:
             print("\033[31mCampo obrigatório!\033[0;0m")
+            continue
         # Verifica se o nome de usuário contém espaços
         if " " in usuario:
             print("\033[31mO nome de usuário não pode conter espaços!\033[0;0m")
+            continue
         # Verifica se o nome de usuário contém apenas letras, números e os seguintes caracteres: . _ -
-        if (re.match(r'[^a-zA-Z0-9._-]', usuario)):
+        if (re.search(r'[^a-zA-Z0-9._-]', usuario)):
             print("\033[31mO nome de usuário pode conter apenas letras, números e os seguintes caracteres: . _ -!\033[0;0m")
+            continue
         
         # Retorna o nome de usuário caso ele seja válido
         return usuario
@@ -162,9 +193,11 @@ def NovoUsuario(usuario_atual):
         # Verifica se o nome de usuário contém espaços
         if " " in usuario_input:
             print("\033[31mO nome de usuário não pode conter espaços!\033[0;0m")
+            continue
         # Verifica se o nome de usuário contém apenas letras, números e os caracteres: . _ -
         if (re.match(r'[^a-zA-Z0-9._-]', usuario_input)):
             print("\033[31mO nome de usuário pode conter apenas letras, números e os seguintes caracteres: . _ -!\033[0;0m")
+            continue
         
         # Retorna o nome de usuário caso ele seja válido
         return usuario_input
